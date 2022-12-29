948709a6da63ea364f0837a11de9a099d4ae6e02
Guilherme Setim <64696390+guilhermesetim@users.noreply.github.com>
2022-12-29 19:16:07 -0300

    modelagem da interface

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function enviarParametroTabela(){
     var linhasUsr = Number(document.querySelector('#linhas').value);
     var colunasUsr = Number(document.querySelector('#colunas').value);
-
-    titulo = "<label>Titulo da tabela</label><input type='text' id='captionTabela'><br>";
-    label = "<label>Label da tabela</label><input type='text' id='labelTabela'>";
-    tabela = criacaoTabela(linhasUsr, colunasUsr);
-    campoEstilo = htmlEstiloTabela();
     
 
+    titulo = "<p class='inputsText'><label class='headerInfo'>Titulo da tabela:</label><input type='text' id='captionTabela' placeholder='\\caption{}'></p>";
+    label = "<p class='inputsText'><label class='headerInfo'>Label da tabela:</label><input type='text' id='labelTabela' placeholder='\\label{tab:}'></p>";
+    tabela = criacaoTabela(linhasUsr, colunasUsr);
+    campoEstilo = htmlEstiloTabela(linhasUsr, colunasUsr);
+
+    
     document.querySelector('#respostaDados').innerHTML = (titulo+label+tabela+campoEstilo);
-    document.querySelector('#bt-converter').innerHTML = `<input type='button' value='Converter Valores' onclick='codigo(${linhasUsr},${colunasUsr})'>`;
 
 }
 
@@ -24,7 +24,7 @@ function codigo(qLinhas, qColunas) {
     let escolha = document.querySelector('input[name="estilo"]:checked').value; 
     
   
-    resultado = "<textarea rows='10' cols='40' maxlength='500'>";
+    resultado = `<textarea rows='${qLinhas+11}' cols='40' maxlength='500' class='codLatex'>`;
     resultado += codigoLatex(qLinhas, qColunas, escolha);
     resultado+="</textarea>";
 
@@ -85,19 +85,31 @@ function tabEstilo(colunas, _escolha) {
     return _formatoTabular;
 }
 
-function htmlEstiloTabela() {
-    campo = "<fieldset>";
+function htmlEstiloTabela(linhasUsr, colunasUsr) {
+    campo = "<fieldset class='fieldEstiloTab'>";
     campo+="<legend>Estilo da tabela:</legend>"
-    campo += `<input type="radio" name="estilo" id="tab" value="t" checked>`;
+    campo += "<div class='estiloTabJs'>"; 
+
+    campo += "<div>";    
+    campo += `<p><input type="radio" name="estilo" id="tab" value="t" checked>`;
     campo += `<label for="tab">&#9636 Tabela </label>`;
     campo += `<input type="radio" name="estilo" id="quad" value="q">`;
-    campo += `<label for="quad">&#9638 Quadro </label>  <br>`;
-    campo += `<input type="checkbox" name="propTab" id="cabecalhoTab" value="cabecalho">`;
-    campo += `<label for="cabecalhoTab">Possui cabeçalho</label> <br>`;
-    campo += `<input type="checkbox" name="propTab" id="primLinhaNeg" value="primLinha">`;
-    campo += `<label for="primLinhaNeg">Primeira linha em negrito</label> <br>`;
-    campo += `<input type="checkbox" name="propTab" id="primColunaNeg" value="primColuna">`;
-    campo += `<label for="primColunaNeg">Primeira coluna em negrito</label>`;
+    campo += `<label for="quad">&#9638 Quadro </label>  </p>`;
+
+    campo += `<p><input type="checkbox" name="propTab" id="cabecalhoTab" value="cabecalho">`;
+    campo += `<label for="cabecalhoTab">Possui cabeçalho</label> </p>`;
+
+    campo += `<p><input type="checkbox" name="propTab" id="primLinhaNeg" value="primLinha">`;
+    campo += `<label for="primLinhaNeg">Primeira linha em negrito</label> </p>`;
+
+    campo += `<p><input type="checkbox" name="propTab" id="primColunaNeg" value="primColuna">`;
+    campo += `<label for="primColunaNeg">Primeira coluna em negrito</label></p>`;
+    campo += "</div>";
+
+    campo += `<div><input type='button' value='Converter Valores' onclick='codigo(${linhasUsr},${colunasUsr})'></div>`;
+
+    campo += "</div>";
+
     campo += `</fieldset>`;
 
     return campo;
@@ -120,8 +132,8 @@ function criacaoTabela(linhasUsr, colunasUsr) {
 function codigoLatex(qLinhas, qColunas, escolha) {
 
     let cab = false;
-    let pl = false;
-    let pc = false;
+    let pLinha = false;
+    let pColuna = false;
 
     let propriedades = document.getElementsByName('propTab');
 
@@ -130,9 +142,9 @@ function codigoLatex(qLinhas, qColunas, escolha) {
             if(propriedades[i].value == "cabecalho"){
                 cab = true;
             }else if (propriedades[i].value == "primLinha") {
-                pl = true;
+                pLinha = true;
             }else if (propriedades[i].value == "primColuna") {
-                pc = true;
+                pColuna = true;
             }
         }
     }
@@ -143,13 +155,13 @@ function codigoLatex(qLinhas, qColunas, escolha) {
             for(c = 0; c < qColunas; ++c) {
                 let valor = "";
 
-                if(pl && l == 0){valor+="\\textbf{";}
-                if(pc && c == 0){valor+="\\textbf{";}
+                if(pLinha && l == 0){valor+="\\textbf{";}
+                if(pColuna && c == 0){valor+="\\textbf{";}
 
                 valor += document.querySelector(`#v${l}${c}`).value
 
-                if(pl &&  l == 0) {valor+="}";}
-                if(pc && c == 0){valor+="}";}
+                if(pLinha &&  l == 0) {valor+="}";}
+                if(pColuna && c == 0){valor+="}";}
 
                 if(c < qColunas-1) {resultado+= `${valor} & `; }
                 else {resultado+= valor;}               
